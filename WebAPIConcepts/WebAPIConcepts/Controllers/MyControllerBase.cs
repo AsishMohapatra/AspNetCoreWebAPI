de5f41360f05133ad72cdd15b2c90026f6f08b7f
Asish <mohapatra.asish@gmail.com>
2022-01-26 02:07:03 +0800

    SwashBuckle Customization from Xml Comments

---
+++ WebAPIConcepts/WebAPIConcepts/Controllers/MyControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIConcepts.Controllers
+{
+    /// <summary>
+    /// Base Class for all the controllers
+    /// </summary>
+    //[Route("api/[controller]")] //Base For All Controller with no action
+    [ApiController]
+    public class MyControllerBase : ControllerBase
+    {
+    }
+}
